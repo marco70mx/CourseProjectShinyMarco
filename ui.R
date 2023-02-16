0259024b2801766a4dd10e5a3f61aa4437fdd70d
marco70mx <75866820+marco70mx@users.noreply.github.com>
2023-02-15 21:14:32 -0600

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,40 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Load Data
+
+data("swiss")
+
+# Define UI for application that draws a histogram
+fluidPage(
+
+    # Application title
+    titlePanel("Course Project Shiny Marco Lopez. Using  data frame swiss"),
+
+    # Generate a row with a sidebar 
+    sidebarLayout(       
+      
+      
+      # Define the sidebar with one input 
+      sidebarPanel(p(strong("Documentation:",style="color:red"), a("User Help Page",href="http://rpubs.com/joanperez/293126")), 
+                   selectInput("tema", "Tema:",  
+                               choices=colnames(swiss)), 
+                   hr(), 
+                   helpText("Standardized fertility Switzerland") 
+      ), 
+      
+      # Create a spot for the barplot 
+      mainPanel( 
+        plotOutput("Fer")   
+      ) 
+      
+    ) 
+) 
